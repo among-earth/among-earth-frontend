2ef9c1c99fe7827399fa1962303a68180ec03630
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-12-03 17:54:51 +0900

    :lipstick: Set global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+
 import TravelInfo from './TravelInfo';
 import LandingPage from './LandingPage';
 import UserInfo from './UserInfo';
@@ -9,26 +12,33 @@ import Directions from './Directions';
 import Travel from './Travel';
 import Travels from './Travels';
 
+import theme from './styles/theme';
+import GlobalStyle from './styles/globalStyle';
+
 function App({ user, setUserNickname }) {
   return (
-    <Switch>
-      <Route exact path='/'>
-        {user ? <TravelInfo /> : <LandingPage />}
-      </Route>
-      <Route path='/users'>
-        <UserInfo onLogin={setUserNickname} />
-      </Route>
-      <Route path='/directions'>
-        <Directions />
-      </Route>
-      <Route path='/travel/:travel_id'>
-        <Travel />
-      </Route>
-      <Route path='/travels'>
-        <Travels />
-      </Route>
-      <Redirect to='/' />
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <GlobalStyle />
+      <Switch>
+        <Route exact path='/'>
+          {user ? <TravelInfo /> : <LandingPage />}
+        </Route>
+        <Route path='/users'>
+          <UserInfo onLogin={setUserNickname} />
+        </Route>
+        <Route path='/directions'>
+          <Directions />
+        </Route>
+        <Route path='/travel/:travel_id'>
+          <Travel />
+        </Route>
+        <Route path='/travels'>
+          <Travels />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </ThemeProvider>
   );
 }
 
