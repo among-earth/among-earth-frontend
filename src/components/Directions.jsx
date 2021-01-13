796d139fa7975a92516c4317a750a91beb93c6f6
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-13 13:16:37 +0900

    :zap: update component

--- src/components/Directions.jsx
+++ src/components/Directions.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import styled from 'styled-components';
 
-import RegionSearchInput from './RegionSearchInput';
-import LandmarkSearchInput from './LandmarkSearchInput';
+import RegionSearch from './RegionSearch';
+import LandmarkSearch from './LandmarkSearch';
 import Maps from './Maps';
-import { fetchNearestPlacesFromGoogle, postAllPoints } from '../utils/api';
+import Button from './Button';
 import Modal from './Modal';
 import Header from './Header';
 import Footer from './Footer';
+import { getNearestPlacesFromGoogle } from '../utils/api';
 
-import { GrLocation } from 'react-icons/gr';
-
-const Directions = ({
+function Directions({
   user,
   country,
   landmarkList,
@@ -24,16 +23,13 @@ const Directions = ({
   points,
   getAllPoints,
   travelId,
-}) => {
-  const history = useHistory();
-  const [countryCode, setCountryCode] = useState('');
-  const [isCountrySelected, setCountrySelect] = useState(false);
-  const [landmarkInputValue, setLandmarkInputValue] = useState('');
-  const [countryInputValue, setCountryInputValue] = useState('');
+}) {
   const [recommendList, setRecommendList] = useState([]);
+  const [isCountrySelected, setCountrySelect] = useState(false);
   const [isLandmarkSelected, setLandmarkSelect] = useState(false);
-  const [isDisabled, setDisabled] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
+
+  console.log(isModalOpen);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
@@ -41,8 +37,7 @@ const Directions = ({
   useEffect(() => {
     (async () => {
       try {
-        const suggestions = await fetchNearestPlacesFromGoogle(landmarkList);
-
+        const suggestions = await getNearestPlacesFromGoogle(landmarkList);
         const newRecommendList = suggestions.map(suggestion => ({
           name: suggestion.name,
           id: suggestion.place_id,
@@ -51,22 +46,11 @@ const Directions = ({
 
         setRecommendList(newRecommendList);
       } catch (err) {
-        console.log(err);
+        const { response } = err;
+        if (response) alert('추천 경유지를 불러오는데 실패했습니다. 다시 시도해주세요.');
       }
     })();
   }, [isLandmarkSelected]);
-
-  const handleClick = ev => {
-    const { name, id, value, className} = ev.target;
-
-    selectLandmark([{
-      name: name,
-      id: id,
-      coordinates: { lat: Number(value), lng: Number(className) },
-    }]);
-
-    if (landmarkList.length === 4) setDisabled(true);
-  };
 
   const submitTravelData = ev => {
     ev.preventDefault();
@@ -74,107 +58,62 @@ const Directions = ({
     setModalOpen(true);
   };
 
-  console.log(points)
-
-  const handleModalButton = () => {
-    history.push(`/travels/${travelId}`,{ points: points, trvelId: travelId});
-    // try {
-    //   await postAllPoints(points, travelId);
-
-    //   history.push(`/travels/${travelId}`,{ points: points, trvelId: travelId});
-    // } catch (err) {
-    //   console.log(err);
-    // }
-  };
-
   return (
     <Container>
       <Header />
-      <form onSubmit={submitTravelData}>
+      <FormWrapper onSubmit={submitTravelData}>
         {isCountrySelected ?
-          <LandmarkWrapper>
-            <p><span>{country}</span>에서 가고 싶은 장소를 입력해 주세요.</p>
-            <LandmarkSearchInput
-              inputValue={landmarkInputValue}
-              countryCode={countryCode}
-              setInputValue={setLandmarkInputValue}
-              landmarkList={landmarkList}
-              selectLandmark={selectLandmark}
-              setNearLandmark={fetchNearestPlacesFromGoogle}
-              setRecommendList={setRecommendList}
-              setLandmarkSelect={setLandmarkSelect}
-              disabled={isDisabled}
-              setDisabled={setDisabled}
-            />
-          </LandmarkWrapper> :
-          <RegionWrapper>
-            <p>안녕하세요<span>{user.nickname}님,</span></p>
-            <span>어느 나라로 가볼까요?</span>
-            <RegionSearchInput
-              country={country}
-              setCountry={selectCountry}
-              setCountryCode={setCountryCode}
-              setCountrySelect={setCountrySelect}
-              inputValue={countryInputValue}
-              setInputValue={setCountryInputValue}
-            />
-          </RegionWrapper>
+          <LandmarkSearch
+            landmarkList={landmarkList}
+            selectLandmark={selectLandmark}
+            setLandmarkSelect={setLandmarkSelect}
+            country={country}
+            recommendList={recommendList}
+            openModal={openModal}
+            isLandmarkSelected={isLandmarkSelected}
+          />
+        :
+          <RegionSearch
+            user={user}
+            setCountry={selectCountry}
+            setCountrySelect={setCountrySelect}
+          />
         }
-      </form>
-      <Recommends>
-        {isLandmarkSelected &&
-          <>
-          <div>근처에 있는 추천 장소</div>
-          {recommendList && recommendList.map(recommend => {
-            return (
-              <button
-                type='button'
-                id={recommend.id}
-                name={recommend.name}
-                onClick={handleClick}
-                key={recommend.id}
-                value={recommend.coordinates.lat}
-                className={recommend.coordinates.lng}
-              >
-                {recommend.name}
-              </button>
-            );
-          })}
-          </>
-        }
-      </Recommends>
-      <SelectedLandmarkWrapper>
-        {landmarkList && landmarkList.map(landmark => {
-          return (
-          <div key={landmark.id}>
-            <span>{landmark.name}</span>
-            <GrLocation fontSize='24px' style={{ color: '#eba13d'}}/>
-          </div>
-          );
-        })}
-      </SelectedLandmarkWrapper>
-      <button onClick={openModal}>확인</button>
-      {isModalOpen &&
+      </FormWrapper>
+      {isModalOpen && (
         <Modal
           isModalOpen={isModalOpen}
           closable={true}
           maskClosable={true}
           onClose={closeModal}
+          travelId={travelId}
         >
+          <h1>이 경로로 여행을 시작할까요?</h1>
+          <h3>총 거리는 {totalDistance}m 입니다.</h3>
           <Maps
             landmarkList={landmarkList}
             setTotalDistance={calculateTotalDistance}
             setPoints={getAllPoints}
           />
-          <button onClick={handleModalButton}>좋아요</button>
+          <Button path={`/travels/${travelId}`} isLanding={false}>Lets go!</Button>
         </Modal>
-      }
+      )}
       <Footer />
     </Container>
   );
-};
+}
+
+const FormWrapper = styled.form`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
 
 const Container = styled.div`
+  position: relative;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -193,97 +132,12 @@ const Container = styled.div`
     padding: 0 20px;
     height: 50px;
     border-radius: 6px;
-    outline: none;
 
     &:focus {
     background: #f7efeb;
     }
   }
-
-  input[type='submit'] {
-    width: 60px;
-    cursor: pointer;
-    text-align: center;
-    background-color: ${({theme}) => theme.orangeYellow};
-    height: 50px;
-    border-radius: 6px;
-    color: ${({theme}) => theme.ivory};
-    font-family: 'Limelight', cursive;
-  }
 `;
-
-const SelectedLandmarkWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 80px;
-  width: 100%;
-
-  div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 0 30px;
-  }
-
-  span {
-    margin-bottom: 20px;
-    font-size: 14px;
-  }
-`;
-
-const RegionWrapper = styled.div`
-  p {
-    margin-bottom: 8px;
-    color: ${({theme}) => theme.ivory};
-  }
-
-  span {
-    color: ${({theme}) => theme.ivory};
-  }
-
-  span:first-child {
-    font-size: 24px;
-    margin: 0 10px;
-  }
-`;
-
-const LandmarkWrapper = styled.div`
-  margin: 40px 0;
-
-  p {
-    margin-bottom: 8px;
-    color: ${({theme}) => theme.ivory};
-  }
-
-  span {
-    color: ${({theme}) => theme.ivory};
-  }
-
-  span:first-child {
-    font-size: 24px;
-    margin-right: 10px;
-  }
-`;
-
-const Recommends = styled.div`
-  width: 500px;
-
-  div {
-    color: ${({theme}) => theme.ivory};
-  }
-
-  button {
-    padding: 5px 10px;
-    border-radius: 20px;
-    border: none;
-    margin: 10px;
-    background-color: ${({theme}) => theme.ivory};
-    color: ${({theme}) => theme.green}
-  }
-`;
-
 
 export default Directions;
 
@@ -301,27 +155,3 @@ Directions.propTypes = {
   getAllPoints: PropTypes.func.isRequired,
   travelId: PropTypes.string.isRequired,
 };
-
-// FIXME: image 가져오기
-// const newRecommendList = await suggestions.map(async suggestion => {
-//   try {
-//   const photoRef = suggestion.photos[0].photo_reference;
-//   let url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photoRef}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
-//   let res = await fetch(url, {
-//     credentials: 'includes',
-//   });
-//   const blob = new Blob();
-//   const result = res.blob();
-
-//   const image = URL.createObjectURL(result);
-
-//   return ({
-//     name: suggestion.name,
-//     id: suggestion.place_id,
-//     coordinates: suggestion.geometry.location,
-//     photo: image,
-//   })
-// } catch (err) {
-//   console.log(err);
-// }
-// });
