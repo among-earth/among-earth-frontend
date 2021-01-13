6debcf08279a996c391ad3718b85272bdd98d1c3
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-13 13:32:21 +0900

    :recycle: refactor code

--- src/containers/DirectionsContainer.js
+++ src/containers/DirectionsContainer.js
@@ -1,15 +1,15 @@
 import { connect } from 'react-redux';
 
-import { calculateTotalDistance, selectCountry, selectLandmark, getAllPoints, setTravelId } from '../redux/directions/directions.actions';
+import { calculateTotalDistance, selectCountry, selectLandmark, getAllPoints } from '../redux/directions/directions.actions';
 
 import Directions from '../components/Directions';
 
 const mapStateToProps = state => ({
-    country: state.directions.country,
-    landmarkList: state.directions.landmarkList,
-    totalDistance: state.directions.totalDistance,
-    points: state.directions.points,
-    travelId: state.directions.id,
+  country: state.directions.country,
+  landmarkList: state.directions.landmarkList,
+  totalDistance: state.directions.totalDistance,
+  points: state.directions.points,
+  travelId: state.directions.id,
 });
 
 const mapDispatchToProps = dispatch => ({
