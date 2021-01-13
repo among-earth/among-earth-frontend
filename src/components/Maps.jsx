94d402054d4e545b1b8b69d6e4e814307c65b58b
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-13 13:27:25 +0900

    :recycle: refactor code

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -3,11 +3,9 @@ import PropTypes from 'prop-types';
 
 import { GoogleMap, DirectionsRenderer } from '@react-google-maps/api';
 
-import { romeData, newYorkData } from '../lib/landmarkMockData';
+import { computeTotalDistance } from '../utils';
 
-import { computeTotalDistance } from '../utils/directions';
-
-const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
+function Maps({ landmarkList, setTotalDistance, setPoints }) {
   const [map, setMap] = useState(null);
   const [directions, setDirections] = useState(null);
   const [markers, setMarkers] = useState(false);
@@ -17,19 +15,20 @@ const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
     const directionsService = new google.maps.DirectionsService();
     const directionsDisplay = new google.maps.DirectionsRenderer();
 
-    const origin = new google.maps.LatLng(newYorkData[0].coordinates.lat, newYorkData[0].coordinates.lng);
-    const destination = new google.maps.LatLng(newYorkData[newYorkData.length - 1].coordinates.lat, newYorkData[newYorkData.length - 1].coordinates.lng);
-    // TODO: comback
-    // const waypoints = [];
-    // const routes = newYorkData.splice(1, 3);
+    const origin = new google.maps.LatLng(landmarkList[0].coordinates.lat, landmarkList[0].coordinates.lng);
+    const destination = new google.maps.LatLng(landmarkList[landmarkList.length - 1].coordinates.lat, landmarkList[landmarkList.length - 1].coordinates.lng);
 
-    // for(let i = 0; i < routes.length; i++) {
-    //   const location = { location: new google.maps.LatLng(routes[i].coordinates.lat, routes[i].coordinates.lng)};
-    //   waypoints.push(location);
-    // }
     const waypoints = [];
-    const location = { location: new google.maps.LatLng(newYorkData[1].coordinates.lat, newYorkData[1].coordinates.lng) };
-    waypoints.push(location);
+
+    const routes = landmarkList.splice(1, 3);
+
+    for(let i = 0; i < routes.length; i++) {
+      const location = { location: new google.maps.LatLng(routes[i].coordinates.lat, routes[i].coordinates.lng)};
+      waypoints.push(location);
+    }
+    // const waypoints = [];
+    // const location = { location: new google.maps.LatLng(landmarkList[1].coordinates.lat, landmarkList[1].coordinates.lng) };
+    // waypoints.push(location);
 
     const request = {
       origin: origin,
@@ -43,21 +42,8 @@ const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
       console.log(result, 'result');
       if (status === google.maps.DirectionsStatus.OK) {
         const totalDistance = computeTotalDistance(result);
-        // const { overview_path } = result.routes[0];
-
-        // for (let i = 0; i < overview_path.length - 1; i++) {
-        //   const start = overview_path[i];
-        //   const end = overview_path[i + 1];
-        //   const overview_head = google.maps.geometry.spherical.computeHeading(start, end);
-
-        //   setPoints([{
-        //     head: overview_head,
-        //     lat: overview_path[i].lat(),
-        //     lng: overview_path[i].lng(),
-        //   }]);
-        // }
-
         const { legs } = result.routes[0];
+        let copyPoints = [];
 
         for (let i = 0; i < legs.length; i++) {
           const steps = legs[i].steps;
@@ -67,26 +53,17 @@ const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
             const end = steps[j].end_location;
             const heading = google.maps.geometry.spherical.computeHeading(start, end);
 
-            // TODO: delete cutSegment
-            const cutSegment = nextSegment.slice(1, 5);
-            console.log(cutSegment, 'cutSegment')
-            cutSegment.forEach(segment => {
-              setPoints([{
+            nextSegment.forEach(segment => {
+              copyPoints.push({
                 head: heading,
                 lat: segment.lat(),
                 lng: segment.lng(),
-              }]);
+              });
+
             });
-            // for (let k = 0; k < nextSegment.length; k++) {
-            //   setPoints([{
-            //     head: heading,
-            //     lat: nextSegment[i].lat(),
-            //     lng: nextSegment[i].lng(),
-            //   }]);
-            // }
           }
         }
-
+        setPoints(copyPoints);
         setDirections(result);
         setMarkers(true);
         setTotalDistance(totalDistance);
@@ -110,18 +87,25 @@ const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
     },
   };
 
-  const mapOptions = {
-    width: '400px',
+  const styleOptions = {
+    width: '600px',
     height: '400px',
+    borderRadius: '10px',
   };
 
+  const mapOptions = {
+    mapTypeId: 'satellite',
+    streetViewControl: false,
+    mapTypeControl: false,
+  };
 
   return (
     <GoogleMap
-      mapContainerStyle={mapOptions}
-      zoom={10}
+      mapContainerStyle={styleOptions}
+      zoom={13}
       onLoad={onLoad}
       onUnmount={onUnmount}
+      options={mapOptions}
     >
       {directions && (
         <DirectionsRenderer
@@ -132,7 +116,7 @@ const Maps = ({ landmarkList, setTotalDistance, setPoints }) => {
       )}
     </GoogleMap>
   );
-};
+}
 
 export default memo(Maps);
 
@@ -141,63 +125,3 @@ Maps.propTypes = {
   setTotalDistance: PropTypes.func.isRequired,
   setPoints: PropTypes.func.isRequired,
 };
-
-  // const origin = new google.maps.LatLng(landmarkList[0].coordinates.lat, landmarkList[0].coordinates.lng);
-  // const destination = new google.maps.LatLng(landmarkList[landmarkList.length - 1].coordinates.lat, landmarkList[landmarkList.length - 1].coordinates.lng);
-  // const waypoints = [];
-  // const routes = landmarkList.splice(1, 3);
-  // for(let i = 0; i < routes.length; i++) {
-  //   const location = { location: new google.maps.LatLng(routes[i].coordinates.lat, routes[i].coordinates.lng) };
-  //   // const placeId = { placeId: routes[i].placeId };
-  //   waypoints.push(location);
-  // }
-
-  // const caldistance = (google.maps.geometry.spherical.computeDistanceBetween(start, end) / 1000).toFixed(5);
-
-  // 직선 사이에 미터당 거리를 구할 수 있으면 heading을 그냥 미터당으로 넘겨주는것도 좋은 방법..
-  // google.maps.Polyline.prototype.GetPointsAtDistance = function(metres) {
-  //   const next = metres;
-  //   const points = [];
-  //   // some awkward special cases
-  //   if (metres <= 0) return points;
-  //   const dist = 0;
-  //   const olddist = 0;
-  //   for (let i = 1; i < this.getPath().getLength()); i++) {
-  //     olddist = dist;
-  //     dist += google.maps.geometry.spherical.computeDistanceBetween(this.getPath().getAt(i), this.getPath().getAt(i - 1));
-  //     while (dist > next) {
-  //       const p1 = this.getPath().getAt(i - 1);
-  //       const p2 = this.getPath().getAt(i);
-  //       const m = (next - olddist) / (dist - olddist);
-  //       points.push(new google.maps.LatLng(p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m));
-  //       next += metres;
-  //     }
-  //   }
-  //   return points;
-  // }
-
-
-  // More delicate segments----------------------------------------------
-  // for (let i = 0; i < legs.length; i++) {
-  //   const steps = legs[i].steps;
-  //   for (let j = 0; j < steps.length; j++) {
-  //     const nextSegment = steps[j].path;
-  //     const start = steps[j].start_location;
-  //     const end = steps[j].end_location;
-  //     const heading = google.maps.geometry.spherical.computeHeading(start, end);
-
-  //     // points.push({
-  //     //   path: nextSegment,
-  //     //   head: heading,
-  //     // });
-
-  //     for (let k = 0; k < nextSegment.length; k++) {
-  //       points.push({
-  //         lat: nextSegment[k].lat(),
-  //         lng: nextSegment[k].lng(),
-  //         head: heading,
-  //     });
-  //     }
-  //   }
-  // }
-  // ----------------------------------------------------------------------
