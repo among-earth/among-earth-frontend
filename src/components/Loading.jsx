19bc35a03c944d7b38daede018910b4fe8d53ed0
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-13 13:22:29 +0900

    :sparkles: create Loading component for lazy, suspense

---
+++ src/components/Loading.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import ReactLoading from 'react-loading';
+
+import theme from './styles/theme';
+
+import Header from './Header';
+import Footer from './Footer';
+
+function Loading() {
+  return (
+    <Wrapper>
+      <Header />
+      <h1>Loading...</h1>
+      <ReactLoading type='balls' color={theme.orangeYellow} width={'5%'} height={'5%'} />
+      <Footer />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    font-family: 'Limelight', cursive;
+    color: ${({theme}) => theme.orangeYellow};
+    margin-bottom: 30px;
+    font-size: 30px;
+  }
+`;
+
+export default Loading;
