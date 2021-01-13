8c749426f8a8ce70dabdf324d1c5e673fbfb86c5
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2021-01-13 13:34:54 +0900

    :recycle: seperate business logic

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+const selectLastWord = value => {
+  const splittedArray = value.split(', ');
+  const len = splittedArray.length;
+
+  return splittedArray[len - 1];
+};
+
+const computeTotalDistance = result => {
+  let total = 0;
+  const myroute = result.routes[0];
+  for (let i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+  total = total / 1000;
+
+  return total;
+};
+
+export { selectLastWord, computeTotalDistance };
